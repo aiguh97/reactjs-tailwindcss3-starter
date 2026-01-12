6feedfbc69518b4509adbae9c7848f3d8f385536
minguh <aiguh97@gmail.com>
2026-01-12 23:38:08 +0700

    adding aggain

--- src/pages/Submenu2.jsx
+++ src/pages/Submenu2.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Dashboard from '../layouts/Dashboard'
 
 const Submenu2 = () => {
   return (
     <div>
+        <Dashboard/>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi explicabo enim tenetur consequuntur, hic adipisci ducimus, ea nulla fugit id eveniet doloremque animi aspernatur a et dicta eius soluta odio!
     </div>
   )
